07e8b81e28bc13b4b15c5205b7a15abc883a65e6
ironpro <darlingboxing@gmail.com>
2021-06-26 03:19:11 +0800

    update - password | username | activate

--- Validate/myscripts.js
+++ Validate/myscripts.js
@@ -9,10 +9,36 @@ let Password_type = document.getElementById("password_hiden_text").type;
 //
 var username = false;
 var password = false;
+var firstname = false;
+var lastname = false;
+
+//Enable/Disabl Submit Button
+const EnableOrDisable = ()=>{
+    console.log(password);
+    let Mem_Mobile = document.getElementById('Mem_Mobile').value;
+    let Referral_UserName = document.getElementById('Referral_UserName').value;
+    let flag  = false;
+    for (var k in data["words"]) {
+        if(data["words"][k] == Referral_UserName){
+            flag = true;
+        }
+    }
+
+    if(!username || !password|| !firstname||!lastname||flag||Mem_Mobile.length == 0||Referral_UserName.length==0){
+  
+        document.getElementById('btn-submit').disabled = true;
+        document.getElementById('btn-submit').style.background="grey";
+    }else{
+  
+        document.getElementById('btn-submit').disabled = false;
+        document.getElementById('btn-submit').style.background="blue";
+    }
+};
+EnableOrDisable();
+// end
+
+
 // validate phone number start 
-
-
-
 const formatToPhone = (event) => {
     if(isModifierKey(event)) {return;}
 
@@ -24,6 +50,8 @@ const formatToPhone = (event) => {
     if(input.length > 6){event.target.value = `(${areaCode}) ${middle} - ${last}`;}
     else if(input.length > 3){event.target.value = `(${areaCode}) ${middle}`;}
     else if(input.length > 0){event.target.value = `(${areaCode}`;}
+    EnableOrDisable();
+
 };
 const isNumericInput = (event) => {
     const key = event.keyCode;
@@ -56,6 +84,37 @@ Mem_Mobile.addEventListener('keydown',enforceFormat);
 Mem_Mobile.addEventListener('keyup',formatToPhone);
 // validate phone number end 
 
+
+//validate the Mem_F_Name,Mem_L_Name start
+
+
+
+const Mem_F_Name  = document.getElementById('Mem_F_Name');
+const Mem_L_Name  = document.getElementById('Mem_L_Name');
+
+
+const firstNameCheck  = (value)=>{
+    if (document.getElementById('Mem_F_Name').value.length < 30 && document.getElementById('Mem_F_Name').value.length > 2) {
+        firstname = true;
+      
+   } else {
+        firstname = false;
+   }
+   EnableOrDisable();
+};
+const lastNameCheck = ()=>{
+    if (document.getElementById('Mem_L_Name').value.length < 30 && document.getElementById('Mem_L_Name').value.length > 2) {
+        lastname = true;
+      
+   } else {
+        lastname = false;
+   }
+   EnableOrDisable();
+}
+
+Mem_F_Name.addEventListener('input',firstNameCheck);
+Mem_L_Name.addEventListener('input',lastNameCheck)
+//validate the Mem_F_Name,Mem_L_Name end
 
 //validate user name [Referral_UserName] start
 
@@ -96,11 +155,12 @@ const onCheckuserName = (value)=>{
         document.getElementById('Number_allow_user_name').style["color"] = "black";
     }
 
-    if(character4||letterallow||numberallow||character21){
+    if(character4&&letterallow&&numberallow&&character21){
         username = true;
     }else{
         username = false;
     }
+    EnableOrDisable();
 }
 
 //validate user name [Referral_UserName] end
@@ -121,19 +181,7 @@ function onChangeType(changeType) {
 }
 
 
-function onChangeType(changeType) {
-    document.getElementById("password_hiden_text").type = changeType;
-    if (changeType == "text") {
-        document.getElementById("svg_password_show_text").style.display = "flex";
-        document.getElementById("svg_password_hiden_text").style.display = "none";
-    } else if (changeType == "password") {
-        document.getElementById("svg_password_show_text").style.display = "none";
-        document.getElementById("svg_password_hiden_text").style.display = "flex";
-    } else {
-        document.getElementById("svg_password_show_text").style.display = "none";
-        document.getElementById("svg_password_hiden_text").style.display = "flex";
-    }
-}
+
 
 //validate password start
 const onCheckPassword = (value)=>{
@@ -177,25 +225,33 @@ const onCheckPassword = (value)=>{
     if (new RegExp("^(?=.*[0-9]).+$").test(value)) {
         number = true;
         document.getElementById('at_least_number_chater_password').style["color"] = "#356AB3";
+
     } else {
         number = false;
         document.getElementById('at_least_number_chater_password').style["color"] = "black";
     }
-    if (new RegExp("^(?=.*[!@#\$%\^\&*\)\(`+=._-]).+$").test(value)) {
+    if (new RegExp('^(?=.*[!@#$%^&*"\\[\\]\\{\\}<>/\\(\\)=\\\\\\-_´+`~\\:;,\\.€\\|])').test(value)) {
          special = true;
-        document.getElementById('at_least_special_letter_chater_password').style["color"] = "#356AB3";
+         document.getElementById('at_least_special_letter_chater_password').style["color"] = "#356AB3";
+
+         if(new RegExp("^(?=.*[\\(\\)$<>&^!/\\[\\]*\\\\])").test(value)){
+            special = false;
+            document.getElementById('at_least_special_letter_chater_password').style["color"] = "black";
+         }
     } else {
         special = false;
         document.getElementById('at_least_special_letter_chater_password').style["color"] = "black";
     }
 
-    if(character8||capital||number||special||character33){
+
+    if(character8&&capital&&number&&special&&character33){
+    
         password = true;
     }else{
         password = false;
     }
+    EnableOrDisable();
 }
-
 
 //validate password end
 
@@ -239,4 +295,3 @@ const onSubmit  = ()=>{
     alert("Successfull");
 
 }
-
